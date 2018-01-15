2913b2676ca77d188e135d7b6e5423df16a0ee3e
Artem Zhukov <zhukovgreen@yandex.ru>
2018-01-15 07:58:59 +0100

    Refactoring

--- projects/smartcab/smartcab/rule_based_policy.py
+++ projects/smartcab/smartcab/rule_based_policy.py
@@ -1,43 +1,101 @@
-green_light = 'green'
-red_light = 'red'
-forward = 'forward'
-left = 'left'
-right = 'right'
-no_action = None
+GREEN_LIGHT = 'green'
+RED_LIGHT = 'red'
+FORWARD = 'forward'
+LEFT = 'left'
+RIGHT = 'right'
+NO_ACTION = None
 
 def possible_going_left(light, oncoming_trafic, left_trafic):
-    return light == green_light and oncoming_trafic == left
+    return light == GREEN_LIGHT and oncoming_trafic == LEFT
 
 def possible_going_forward(light, oncoming_trafic, left_trafic):
-    return light == green_light
+    return light == GREEN_LIGHT
 
 def possible_going_right(light, oncoming_trafic, left_trafic):
     return (
-        light == green_light
+        light == GREEN_LIGHT
         ) or (
-        light == red_light and not left_trafic == forward
+        light == RED_LIGHT and left_trafic != FORWARD
         )
 
-def ruled_based_policy(light, waypoint, oncoming_trafic, left_trafic):
-    possible_to_move_towards_waypoint = {
-        forward: possible_going_forward,
-        left: possible_going_left,
-        right: possible_going_right,
-    }
+POSSIBLE_TO_MOVE_TOWARD_WAYPOINT = {
+    FORWARD: possible_going_forward,
+    LEFT: possible_going_left,
+    RIGHT: possible_going_right,
+}
 
-    return waypoint if possible_to_move_towards_waypoint[waypoint](
+VALID_ACTIONS = {
+    ('red', 'left', 'left', 'left'): None,
+    ('red', 'left', 'left', 'right'): None,
+    ('red', 'left', 'left', 'forward'): None,
+    ('red', 'left', 'right', 'left'): None,
+    ('red', 'left', 'right', 'right'): None,
+    ('red', 'left', 'right', 'forward'): None,
+    ('red', 'left', 'forward', 'left'): None,
+    ('red', 'left', 'forward', 'right'): None,
+    ('red', 'left', 'forward', 'forward'): None,
+    ('red', 'right', 'left', 'left'): 'right',
+    ('red', 'right', 'left', 'right'): 'right',
+    ('red', 'right', 'left', 'forward'): None,
+    ('red', 'right', 'right', 'left'): 'right',
+    ('red', 'right', 'right', 'right'): 'right',
+    ('red', 'right', 'right', 'forward'): None,
+    ('red', 'right', 'forward', 'left'): 'right',
+    ('red', 'right', 'forward', 'right'): 'right',
+    ('red', 'right', 'forward', 'forward'): None,
+    ('red', 'forward', 'left', 'left'): None,
+    ('red', 'forward', 'left', 'right'): None,
+    ('red', 'forward', 'left', 'forward'): None,
+    ('red', 'forward', 'right', 'left'): None,
+    ('red', 'forward', 'right', 'right'): None,
+    ('red', 'forward', 'right', 'forward'): None,
+    ('red', 'forward', 'forward', 'left'): None,
+    ('red', 'forward', 'forward', 'right'): None,
+    ('red', 'forward', 'forward', 'forward'): None,
+    ('green', 'left', 'left', 'left'): 'left',
+    ('green', 'left', 'left', 'right'): 'left',
+    ('green', 'left', 'left', 'forward'): 'left',
+    ('green', 'left', 'right', 'left'): None,
+    ('green', 'left', 'right', 'right'): None,
+    ('green', 'left', 'right', 'forward'): None,
+    ('green', 'left', 'forward', 'left'): None,
+    ('green', 'left', 'forward', 'right'): None,
+    ('green', 'left', 'forward', 'forward'): None,
+    ('green', 'right', 'left', 'left'): 'right',
+    ('green', 'right', 'left', 'right'): 'right',
+    ('green', 'right', 'left', 'forward'): 'right',
+    ('green', 'right', 'right', 'left'): 'right',
+    ('green', 'right', 'right', 'right'): 'right',
+    ('green', 'right', 'right', 'forward'): 'right',
+    ('green', 'right', 'forward', 'left'): 'right',
+    ('green', 'right', 'forward', 'right'): 'right',
+    ('green', 'right', 'forward', 'forward'): 'right',
+    ('green', 'forward', 'left', 'left'): 'forward',
+    ('green', 'forward', 'left', 'right'): 'forward',
+    ('green', 'forward', 'left', 'forward'): 'forward',
+    ('green', 'forward', 'right', 'left'): 'forward',
+    ('green', 'forward', 'right', 'right'): 'forward',
+    ('green', 'forward', 'right', 'forward'): 'forward',
+    ('green', 'forward', 'forward', 'left'): 'forward',
+    ('green', 'forward', 'forward', 'right'): 'forward',
+    ('green', 'forward', 'forward', 'forward'): 'forward',
+}
+
+def rule_based_policy(light, waypoint, oncoming_trafic, left_trafic):
+
+    return waypoint if POSSIBLE_TO_MOVE_TOWARD_WAYPOINT[waypoint](
             light,
             oncoming_trafic,
             left_trafic
-        ) else no_action
+        ) else NO_ACTION
 
-def test_ruled_based_policy():
+def test_rule_based_policy():
     from itertools import product
 
-    lights = (red_light, green_light)
-    waypoints = (left, right, forward)
-    oncoming_trafics = (left, right, forward)
-    left_trafics = (left, right, forward)
+    lights = (RED_LIGHT, GREEN_LIGHT)
+    waypoints = (LEFT, RIGHT, FORWARD)
+    oncoming_trafics = (LEFT, RIGHT, FORWARD)
+    left_trafics = (LEFT, RIGHT, FORWARD)
 
     for case in product(lights, waypoints, oncoming_trafics, left_trafics):
-        print(case, ruled_based_policy(*case))
+        assert rule_based_policy(*case) == VALID_ACTIONS[case]
